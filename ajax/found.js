f6b966ddd9ee0b7e003fd0d1d7375bac28962900
Jessica Yeung <jjyeung@ucsd.edu>
2017-04-19 17:17:12 -0700

    Added front-end and back-end functionality

--- ajax/found.js
+++ ajax/found.js
@@ -1,1 +1,12 @@
 /* TODO: Implement the Found AJAX Handler */
+// we want to store hte data that we found
+module.exports = {
+	 post: function(req,res) {
+	 	// req is the request" client -> server
+	 	// res is the response: server -> client
+	 	req.body.lat = parseFloat(req.body.lat); //inputting a string, but need a float
+	 	req.body.lng = parseFloat(req.body.lng);
+	 	require("./data.json").push(req.body); // everthing the user posts will go into the body
+	 	res.success();                         // push takes an element and add to an array
+	 }
+}
--- ajax/lost.js
+++ ajax/lost.js
@@ -1,1 +1,16 @@
 /* TODO: Implement the lost AJAX Handler */
+module.exports = {
+	// get is an action and there can be many actions
+	 get: function(req,res) {
+	 	// req is the request" client -> server
+	 	// res is the response: server -> client
+	    var data = require("./data.json");
+		res.success(data); 
+	    // 	Try {
+		//  	var data = require("./data.json");
+		//  	res.success(data); 
+		// } catch (ex) {
+		// 	res.error(57, "data not found");
+		// }
+	 }
+}
